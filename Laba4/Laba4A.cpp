9d536e5e4af2d6e3e4eb1b86d08108884aa59861
ilya <jr.ilyas4fun@gmail.com>
2019-11-01 11:01:18 +0300

    Laba4

---
+++ Laba4/Laba4A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+char* myStrncat(char* strDest, const char* strSource, size_t count) {
+	int last = 0;
+	while (strDest[last]) {
+		++last;
+	}	
+	int  n = 0;
+	while (n < count) {
+		int k = 0;
+		while (strSource[k]) {
+			strDest[last] = strSource[k];
+			++k;
+			++last;
+		}
+		++n;
+	}
+	strDest[last] = '\0';
+	return strDest;
+}
+int main() {
+	ifstream in("input.txt");       //in every line of input file must be TWO strings and ONE number
+	ofstream out("output.txt");
+	char str[301], strSource[301]; 
+	int n;
+	while (in >> str) {
+		in >> strSource >> n;
+		out << myStrncat(str, strSource, n) << '\n';
+	}
+	in.close();
+	out.close();
+}
+
---
+++ Laba4/Laba4B.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include<string>
+using namespace std;
+int length(char* str) {
+	int n = 0;
+	while (str[n]) {
+		++n;
+	}
+	return n;
+}
+bool is_number(char* str) {
+	bool is_numb = true;
+	int i = 0;
+	while (str[i]) {
+		if (str[i] < '0' || str[i] > '9') {
+			is_numb = false;
+			break;
+		}
+		++i;
+	}
+	return is_numb;
+}
+bool is_palindrome(char* str) {
+	bool is_pal = true;
+	int n = length(str);
+	for (int i = 0; i < n / 2; ++i) {
+		if (str[i] != str[n - 1 - i]) {
+			is_pal = false;
+			break;
+		}
+	}
+	return is_pal;
+}
+int main() {
+	ifstream in("input.txt");
+	ofstream out("output.txt");
+	char line[301], str[301], tmp[301] = "there is no palindromes";
+	while (in.getline(line, 301)) {
+		int i = 0;
+		int count = 0;
+		while (line[i] && count < 2) {
+			int length = 0;
+			while (line[i] != ' ' && line[i] != '\0') {
+				str[length] = line[i];
+				++i;
+				++length;
+			}
+		    str[length] = '\0';
+
+			if (is_number(str)) {
+				if (is_palindrome(str)) {
+					++count;
+					for (int i = 0; i < length; i++) {  
+						tmp[i] = str[i];
+					}
+					tmp[length] = '\0';
+				}
+			}
+			while (line[i] == ' ') {
+				++i;
+			}
+		}
+		out << tmp <<'\n';
+	}
+in.close();
+out.close();
+}
