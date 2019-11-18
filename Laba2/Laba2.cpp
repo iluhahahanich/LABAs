1174c8cc69c6df12d65427e5d5a7bd2e29b9aacd
ilya <jr.ilyas4fun@gmail.com>
2019-11-18 19:19:12 +0300

    Corrected

--- Laba2/Laba2.cpp
+++ Laba2/Laba2.cpp
@@ -1,30 +1,48 @@
 #include <iostream>
 #include <ctime>
+#include <conio.h>
 using namespace std;
 int main() {
-    int n;
+	int n;
 	cout << " n = ";
 	cin >> n;
 	int const size = 10001;
-	int a[size];
-	for (int i = 0; i < n; ++i) {
-		cout << "a[" << i << "] = ";
-		cin >> a[i];
+	double a[size];
+	cout << "press 'y' to fill array with radom numbers";
+	if (_getch() == 'y') {
+		cout << "\ninterval [x,y] \n";
+		double x, y;
+		cout << "x = ";
+		cin >> x;
+		cout << "y = ";
+		cin >> y;
+		srand(time(0));
+		for (int i = 0; i < n; ++i) {
+			a[i] = (double)rand() / RAND_MAX * (y - x)  + x;
+			cout << "a[" << i << "] = " << a[i] << '\n';
+		}
+	} 
+	else {
+		for (int i = 0; i < n; ++i) {
+			cout << "a[" << i << "] = ";
+			cin >> a[i];
+		}
 	}
-	int p;
+	double p;
 	cout << "p = ";
 	cin >> p;
 	int kol1 = 0;
 	for (int i = 0; i < n; ++i) {
-		if (a[i] < p ) {
+		if (a[i] < p) {
 			++kol1;
 		}
 	}
-	cout <<"the number of a[i] less then '" << p << "' = "  << kol1 << '\n';
-//2nd part
-	int sum = 0;
+	cout << "the number of a[i] less then '" << p << "' = " << kol1 << '\n';
+
+	//2nd part
+	double sum = 0;
 	int i = n - 1;
-	while (( i >= 0) && (a[i] >= 0 )) {
+	while ((i >= 0) && (a[i] >= 0)) {
 		sum += a[i];
 		--i;
 	}
@@ -33,13 +51,13 @@ int main() {
 	}
 	else {
 		if (i == n - 1) {
-			cout << "there are no positive numbers after\n";
+			cout << "there are no positive numbers\n";
 		}
 		else {
 			cout << "the sum after the last negative a[i] = " << sum << "\n";
 		}
 	}
-//3rd part
+	//3rd part
 	int j;
 	for (int i = 0; i < n; ++i) {
 		if (a[i] < p) {
