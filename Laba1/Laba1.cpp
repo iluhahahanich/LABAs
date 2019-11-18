3a165f8cfc85e09876b0b80719f31a66a3cd6a20
ilya <jr.ilyas4fun@gmail.com>
2019-11-18 15:20:59 +0300

    Corrected.

--- Laba1/Laba1.cpp
+++ Laba1/Laba1.cpp
@@ -7,21 +7,19 @@ int main() {
 	cout << "x = ";
 	cin >> x;
 	cout << "k = ";
-	cin	>> k;
+	cin >> k;
 	int a = 1;
-   double  slag = 1, rez = 1;
-	 while (abs(slag)>= pow(10, -k) ) { 
-		slag = slag *(-1)* a / (a + 1) * x;
+	double  slag = 1, rez = 1;
+	while (fabs(slag) >= pow(10, -k)) {
+		slag = slag * (-1) * a / (a + 1) * x;
 		rez += slag;
-		a+=2; 
-	 }
-	cout << "using Talor  " << fixed << setprecision(k) << rez << '\n'; 
-
+		a += 2;
+	}
+	cout << "using Talor  " << fixed << setprecision(k) << rez << '\n';
 	double rez1;
 	rez1 = 1 / sqrt(1 + x);
 	cout << "using formula  " << fixed << setprecision(k) << rez1 << '\n';
-	 
 	double dif;
 	dif = rez - rez1;
-	cout << "difference " << dif; 
+	cout << "difference " << dif;
 }
